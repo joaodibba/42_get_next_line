9894beda38aaf06284fb7fa1e5eba27a21cc51ed
João Miranda <joaomiranda.pessoal@gmail.com>
2023-02-09 15:25:53 +0000

    UPDT

---
+++ get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jalves-c < jalves-c@student.42lisboa.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/30 16:20:05 by jalves-c          #+#    #+#             */
+/*   Updated: 2023/02/09 15:24:50 by jalves-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFFER_SIZE 42
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdbool.h>
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *line, char *buffer);
+size_t	ft_strlen(char *str);
+bool	ft_manage_buffer(char *buffer);
+
+#endif
--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,12 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jalves-c < jalves-c@student.42lisboa.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 19:33:55 by jalves-c          #+#    #+#             */
-/*   Updated: 2023/01/13 19:33:56 by jalves-c         ###   ########.fr       */
+/*   Updated: 2023/02/09 15:22:46 by jalves-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line_bonus.h"
+
+char	*get_next_line(int fd)
+{
+	static char	buffer[FOPEN_MAX][BUFFER_SIZE + 1];
+	char		*line;
+	size_t		index;
+
+	index = 0;
+	if (BUFFER_SIZE < 1 || read(fd, 0, 0) < 0)
+	{
+		if (fd >= 0 && fd <= FOPEN_MAX)
+			while (buffer[fd][index])
+				buffer[fd][index++] = 0;
+		return (0);
+	}
+	line = NULL;
+	while (buffer[fd][0] || read(fd, buffer[fd], BUFFER_SIZE))
+	{
+		line = ft_strjoin(line, buffer[fd]);
+		if (ft_manage_buffer(buffer[fd]))
+			break ;
+	}
+	return (line);
+}
